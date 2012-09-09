90d4d3315554f84c73fc373ba99ebcdf27d17321
Yang Hu <coolcute@gmail.com>
2012-09-09 02:43:39 -0700

    Make it as a script

--- cntv_downloader/Mp4Merger.py
+++ cntv_downloader/Mp4Merger.py
@@ -5,6 +5,7 @@ Created on Sep 9, 2012
 
 @author: coolcute
 '''
+import argparse
 import subprocess
 
 # ls | sort -t- -k2 -n | sed -e 's/^/-cat /g' | tr "\n" " " | xargs MP4Box new.mp4
@@ -15,8 +16,8 @@ class Mp4Merger(object):
     '''
     mergeCmd = "ls | sort -t- -k2 -n | sed -e 's/^/-cat /g' | tr \"\\n\" \" \" | xargs MP4Box "
     def __init__( self, workingDir, fileName):
-      self.workingDir = workingDir
-      self.fileName = fileName
+        self.workingDir = workingDir
+        self.fileName = fileName
       
     def merge(self):
         completeMergeCmd = self.mergeCmd + self.fileName
@@ -26,7 +27,15 @@ class Mp4Merger(object):
 
 # Tester
 def main():
-    mp4Merger = Mp4Merger(r"/Volumes/video/百家讲坛/王立群读宋史/tmp", r"123.mp4")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-mp4-directory', help='the directory contains mp4s')
+    parser.add_argument('-o', '--output-file-name', help='final merged mp4 file name')
+    args = parser.parse_args()
+    
+    inputMp4DirectoryPath = args.input_mp4_directory
+    outputFileName = args.output_file_name
+    # mp4Merger = Mp4Merger(r"/Volumes/video/百家讲坛/王立群读宋史/tmp", r"123.mp4")
+    mp4Merger = Mp4Merger(inputMp4DirectoryPath, outputFileName)
     mp4Merger.merge()
 
 # Main method
